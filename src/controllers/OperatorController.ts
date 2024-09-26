f369c7383761b7bcc880500702bcfe4f899c873a
Egor Kondratev <kondratevegor04@gmail.com>
2024-09-27 00:49:31 +0300

    feat: getOne for operator controller

--- src/controllers/OperatorController.ts
+++ src/controllers/OperatorController.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from 'routing-controllers';
+import {Controller, Get, OnUndefined, Param} from 'routing-controllers';
 import 'reflect-metadata';
 import {pool} from "../config/pgPool";
 import {Operator} from "../models/Operator";
@@ -13,6 +13,19 @@ export class OperatorController {
       return result.rows;
     } catch (err) {
       throw new Error(`Failed to get all operators: ${err}`);
+    }
+  }
+
+  @Get('/operators/:id')
+  @OnUndefined(404)
+  async getOne(@Param('id') id: string): Promise<Operator> {
+    try {
+      const result = await pool.query('SELECT * FROM operator WHERE id = $1', [id]);
+
+      return result.rows[0];
+    }
+    catch (err) {
+      throw new Error(`Failed to get operator: ${err}`);
     }
   }
 }
